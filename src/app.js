83e56a1561157da2c9fd5f68d4e63d854c87af65
Lorenzo <lorypsy@gmail.com>
2021-05-12 17:18:30 +0200

    Added Vue lgenerated link list for topnav & footer sitemap.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,97 @@
-
+var app = new Vue({
+	el: "#app",
+	data: {
+		topNavMenu: [
+			{
+				text: "Courses",
+				href: "courses/",
+				featured: false,
+			},
+			{
+				text: "Course Formats",
+				href: "course-formats/",
+				featured: false,
+			},
+			{
+				text: "Add Course",
+				href: "add-course/",
+				featured: true,
+			},
+			{
+				text: "Pages",
+				href: "pages/",
+				featured: false,
+			},
+			{
+				text: "Demos",
+				href: "demos/",
+				featured: false,
+			},
+		],
+		topNavSocial: [
+			{
+				iconFamily: "fab",
+				iconName: "fa-linkedin",
+				href: "https://www.linkedin.com",
+			},
+			{
+				iconFamily: "fab",
+				iconName: "fa-instagram",
+				href: "https://www.instragram.com",
+			},
+			{
+				iconFamily: "fab",
+				iconName: "fa-facebook",
+				href: "https://www.facebook.com",
+			},
+			{
+				iconFamily: "fab",
+				iconName: "fa-twitter",
+				href: "https://www.twitter.com",
+			},
+		],
+		footerSocial: [
+			{
+				iconFamily: "fab",
+				iconName: "fa-facebook",
+				href: "https://www.facebook.com",
+			},
+			{
+				iconFamily: "fab",
+				iconName: "fa-twitter",
+				href: "https://www.twitter.com",
+			},
+			{
+				iconFamily: "fab",
+				iconName: "fa-instagram",
+				href: "https://www.instragram.com",
+			},
+		],
+		footerSitemap: [
+			{
+				text: "Blog",
+				href: "blog/",
+			},
+			{
+				text: "Courses",
+				href: "courses/",
+			},
+			{
+				text: "Home",
+				href: "home/",
+			},
+			{
+				text: "Membership Area",
+				href: "members/",
+			},
+			{
+				text: "Shortcodes",
+				href: "shortcodes/",
+			},
+			{
+				text: "Typography",
+				href: "typography/",
+			},
+		],
+	},
+});
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,6 @@
 let mix = require("laravel-mix");
 
-mix.js("src/app.js", "dist").setPublicPath("dist");
+mix.js("src/app.js", "dist/").vue({ version: 2 });
 mix.sass("src/app.scss", "dist/").options({
 	processCssUrls: false,
 });
